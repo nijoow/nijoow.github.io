3d52aa2116d9a11597a403cd7a0bc5dbb7f13d50
nijoow <nijoow1127@gmail.com>
2022-02-21 19:40:43 +0900

    footer 업로드

--- components/Footer.js
+++ components/Footer.js
@@ -1,12 +1,21 @@
 const Footer = () => {
   return (
-    <div className="footer">
-      &copy; {new Date().getFullYear()} Lee Woo Jin. All Rights Reserved.
+    <footer className="footer">
+      <div>
+        &copy; {new Date().getFullYear()} Lee Woo Jin. All Rights Reserved.
+      </div>
       <style jsx>{`
         .footer {
+          width: 100%;
+          padding: 20px;
+        }
+        .footer div {
+          text-align: center;
+          margin: auto;
+          color: #a69dc9;
         }
       `}</style>
-    </div>
+    </footer>
   );
 };
 
