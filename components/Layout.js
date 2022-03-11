b8fe335f28e15173bc3e4e30ac88343e267a13c8
nijoow <nijoow1127@gmail.com>
2022-03-12 02:14:54 +0900

    refactor : Layout 구조 변경
    Canvas와 3D object가 모든 페이지에 들어가도록 변경

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,29 @@
 import NavBar from "./NavBar";
 import Footer from "./Footer";
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import Lights from "../components/Lights";
+import Nijoow from "../components/Nijoow";
 
 export default function Layout({ children }) {
   return (
     <>
       <NavBar />
       <container className="container">
+        <div>
+          <Canvas
+            colorManagement
+            camera={{ position: [0, 0, 1], fov: 70 }}
+            className="canvas"
+          >
+            <OrbitControls />
+            <Lights />
+            <Suspense fallback={null} r3f>
+              <Nijoow />
+            </Suspense>
+          </Canvas>
+        </div>
         <div className="contents">{children}</div>
         <Footer />
       </container>
@@ -17,10 +35,13 @@ export default function Layout({ children }) {
           width: 100%;
           min-height: 100vh;
           padding-top: 50px;
-          background: #4b4173;
+          background: #222;
         }
         .contents {
           flex: 1;
+        }
+        .canvas {
+          background: #222;
         }
       `}</style>
     </>
